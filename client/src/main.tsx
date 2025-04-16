06b1dedc894726450ad6c758ffda68625cb60577
Abdul Jabbar <saj8180025653@gmail.com>
2025-04-16 17:23:37 +0530

    Last Last Last commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "sonner";
@@ -7,10 +7,10 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <App />
       <Toaster position="top-right" richColors />
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
